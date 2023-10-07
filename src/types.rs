2ed682909424b69660573a51f3f77bfb3f9b0787
hectorLop <lopez.almazan.hector@gmail.com>
2023-10-07 12:02:47 +0200

    Created types.rs and added tests

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,7 @@
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct PositiveFloat(pub f64);
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct PositiveFloatError(String);
 
 impl TryFrom<f64> for PositiveFloat {
@@ -10,11 +10,36 @@ impl TryFrom<f64> for PositiveFloat {
     fn try_from(value: f64) -> Result<Self, PositiveFloatError> {
         if value < 0.0 {
             return Err(PositiveFloatError(format!(
-                "{} is a negative float.",
+                "{:.2} is a negative float.",
                 value
             )));
         }
 
         Ok(Self(value))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{PositiveFloat, PositiveFloatError};
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_positive_float_creation() {
+        let positive_float = PositiveFloat::try_from(10.0);
+
+        assert!(positive_float.is_ok());
+        assert_eq!(positive_float.unwrap(), PositiveFloat(10.0))
+    }
+
+    #[test]
+    fn test_positive_float_error() {
+        let invalid_float = PositiveFloat::try_from(-10.0);
+
+        assert!(invalid_float.is_err());
+        assert_eq!(
+            invalid_float.unwrap_err(),
+            PositiveFloatError("-10.00 is a negative float.".to_string())
+        );
     }
 }
